6b6279a479b1790319a55c490c08e7eb3e096572
TerrenceWu14 <135181592+TerrenceWu14@users.noreply.github.com>
2023-06-07 13:20:40 +1200

    Add files via upload

---
+++ 02_basic_variables.py
@@ -0,0 +1,14 @@
+# code to learn about variables
+
+# variables can be a string
+greeting = "hello world"
+
+# they can be numbers
+fav_number = 7
+
+# this variable has a problem
+my_name = "Miss G"
+
+print(my_name)
+print(greeting)
+print(fav_number)
